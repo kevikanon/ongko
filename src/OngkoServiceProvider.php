51668b347d758f354af7868ca0d907918e8f4b9e
kevikanon <kevikanon@gmail.com>
2021-06-19 23:04:24 +0600

    Project Initialized
    Project initialized with base files and classes.

---
+++ src/OngkoServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Kevikanon;
+
+use Illuminate\Support\ServiceProvider;
+
+class OngkoServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
